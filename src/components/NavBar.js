24b507807a658431cddfbccbcdf4b227a75fa77e
chucho0rtiz <jesusstivenortiz@gmail.com>
2023-04-13 22:14:08 -0500

    formulario de ver produtos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,16 +51,16 @@ function NavBar() {
                                 <Button color="danger" onClick={toggle}>
                                     Iniciar sesion
                                 </Button>
-                                <Modal isOpen={modal} toggle={toggle}>
+                                {/* Inicio del Modal */}
+                                <Modal isOpen={modal} toggle={toggle} size='lg'>
                                     <ModalHeader toggle={toggle}>Modal title</ModalHeader>
                                     <ModalBody>
-                                        <div class="mb-3">
-                                            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                        <div>
+                                            <label for="inputPassword5" class="form-label">Password</label>
+                                            <input type="password" id="inputPassword5" class="form-control" aria-labelledby="passwordHelpBlock" />
+                                            <div id="passwordHelpBlock" class="form-text">
+                                                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                                            </div>
                                         </div>
                                     </ModalBody>
                                     <ModalFooter>

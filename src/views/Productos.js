9089bab943d846d86da0844f218fec7c1cf066ab
Camilo Orjuela <camilo.orjuela@hotmail.com>
2023-04-14 15:17:35 -0500

    Productos

--- src/views/Productos.js
+++ src/views/Productos.js
@@ -1,4 +1,4 @@
-import '../styles/Style.css';
+import '../styles/StylePro.css';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 
@@ -6,10 +6,103 @@ function Productos() {
     return (
         <div>
             <NavBar />
-            hola
+
+            <img id='img' src='https://picsum.photos/id/123/1200/400'></img>
+
+            <div id='div1'>
+                <input id='buscador' class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search"></input>
+                <button id='boton_buscar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>                
+            </div>
+
+    <section class="contenido">
+        <div class="mostrador" id="mostrador">
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion" id>Nombre del producto </p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+            </div>
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+            </div> 
+            <div class="fila">
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+                <div class="item" onclick="cargar(this)">
+                    <div class="contenedor-foto">
+                        <img src="https://picsum.photos/id/123/1200/400" alt="" />
+                    </div>
+                    <p class="descripcion">Nombre del producto</p>
+                    <p class="precio">Precio: $x.xxx</p>
+                    <button id='btnComprar' class="btn btn-outline-success my-2 my-sm-0" type="submit">Comprar</button>
+                </div>
+            </div> 
+        </div>
+    </section>
+
+
             <Footer />
         </div>
     );
 }
+
+
 
 export default Productos;
